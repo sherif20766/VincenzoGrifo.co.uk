44adb923099f87e941ae632949ed025d9e7fb4b9
Sherif Shawky <sherif20766@hotmail.com>
2016-11-27 20:27:30 +0000

    added another section to home page, rought outline and some animations

---
+++ main.js
@@ -0,0 +1,20 @@
+$(window).scroll(function() {
+		$('#vincenzo').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+
+		$('#vincenzo-intro').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+
+	});
