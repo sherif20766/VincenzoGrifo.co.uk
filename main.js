d6344c2ee85898b66c65e6d753877164f42914da
Sherif Shawky <sherif20766@hotmail.com>
2016-12-04 21:58:56 +0000

    added backstretch.js script, updated header and page animation

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 	
 $(document).ready(function(){
+	$('body').css('display', 'none');
+	$('body').fadeIn(1200);
+
+	
+	$("#front").backstretch([
+      "assets/Background1.jpg",
+      "assets/Background3.jpg",
+      "assets/Background2.jpg",
+      "assets/Background4.jpg",
+      "assets/Background5.jpg",
+      "assets/Background6.jpg",
+      "assets/Background7.jpg"
+
+      ], {
+        fade: 1000,
+        duration: 5000
+    });
+
+
 
 var $header = $("header"),
     $clone  = $header.before($header.clone().addClass("clone"));	
