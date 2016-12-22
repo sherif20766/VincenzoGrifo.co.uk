89afedebd7c1e9afa51ad4f31ddff99f5e2da222
Sherif Shawky <sherif20766@hotmail.com>
2016-12-22 23:07:06 +0000

    started making changes to improve site's responsiveness and started to restructure code

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-	
 $(document).ready(function(){
 	$('body').css('display', 'none');
 	$('body').fadeIn(1200);
 
 	
 	$("#front").backstretch([
-      "assets/Background1.jpg",
-      "assets/Background3.jpg",
-      "assets/Background2.jpg",
-      "assets/Background4.jpg",
       "assets/Background5.jpg",
-      "assets/Background6.jpg",
-      "assets/Background7.jpg"
-
+      "assets/Background7.jpg",
+      "assets/Background2.jpg",
+      "assets/Background4.jpg"
       ], {
         fade: 1000,
         duration: 5000
     });
 
+ 	$(".icon").click(function(){
+ 		$(".menu ul li").css("display","block");
 
-
-var $header = $("header"),
-    $clone  = $header.before($header.clone().addClass("clone"));	
+ 	});
 
 	$(window).scroll(function() {
-
-		var fromTop = $(window).scrollTop();
-            $("body").toggleClass("down", (fromTop > 120));
-
 
 		$('#vincenzo').each(function(){
 		var imagePos = $(this).offset().top;
